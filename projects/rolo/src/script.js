f331e9af4e1fc91cdd06fa0326d6d32ae7a9acee
Kye Buffery <kyebuffery14@gmail.com>
2023-12-15 21:04:55 +0000

    Rolo started

---
+++ projects/rolo/src/script.js
@@ -0,0 +1,127 @@
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+scene.background = ''
+/**
+ * Lights
+ */
+
+const ambientLight = new THREE.AmbientLight(0xfffff)
+scene.add(ambientLight)
+
+
+/**
+ * Objects
+ */
+// Material
+
+const textureLoader = new THREE.TextureLoader();
+const environmentMapTexture = textureLoader.load("/envMap.hdr");
+const metal = textureLoader.load('/metal.png');
+metal.colorSpace = THREE.SRGBColorSpace
+
+const cylinder = new THREE.CylinderGeometry(1.75, 2.25, 2.4, 40, 8, true)
+const material = new THREE.MeshMatcapMaterial({wireframe: true});
+material.matcap = metal;
+const roloSide = new THREE.Mesh(cylinder, material);
+const circleBottom = new THREE.CircleGeometry(2.25)
+const circleTop = new THREE.CircleGeometry(1.75 - .10)
+
+const roloBottom = new THREE.Mesh(circleBottom, material);
+roloBottom.rotation.x = Math.PI * .5;
+roloBottom.position.y = -1.2
+
+const roloTop = new THREE.Mesh(circleTop, material);
+roloTop.rotation.x = - (Math.PI * .5);
+roloTop.position.y = 1
+
+const rolo = new THREE.Group();
+rolo.position.x = -2
+rolo.position.y = -1
+rolo.position.z = -10
+rolo.add( roloSide );
+rolo.add( roloBottom )
+rolo.add( roloTop )
+
+scene.add(rolo);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    rolo.rotation.x = Math.PI * elapsedTime / 8
+    rolo.rotation.y = Math.PI * elapsedTime / 4
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
