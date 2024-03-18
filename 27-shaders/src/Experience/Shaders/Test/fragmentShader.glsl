43a22413b50cbe5ccb604ca683fbf67be3ac8f52
Kye Buffery <kyebuffery14@gmail.com>
2024-03-18 16:37:18 +0000

    Reset flag

--- 27-shaders/src/Experience/Shaders/Test/fragmentShader.glsl
+++ 27-shaders/src/Experience/Shaders/Test/fragmentShader.glsl
@@ -1,8 +1,15 @@
 precision mediump float;
 
-varying float vDeviations;
+// varying float vDeviations;
+    
+// void main()
+// {
+//     gl_FragColor = vec4(vDeviations * .8, vDeviations * .1, vDeviations, 1.0);
+// }
+
+precision mediump float;
     
 void main()
 {
-    gl_FragColor = vec4(vDeviations * .8, vDeviations * .1, vDeviations, 1.0);
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
